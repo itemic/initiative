2ef286c08f80c6cbb27635ff6a0d83b9ecbbad92
Terran Kroft <terrankroft@gmail.com>
2020-04-21 21:04:24 +0800

    Initial Commit

---
+++ Initiative/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Initiative
+//
+//  Created by Terran Kroft on 4/21/20.
+//  Copyright © 2020 itemic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
